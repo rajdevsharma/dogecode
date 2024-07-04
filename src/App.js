a597eef81013fb70708aaa4d4e3da248ab35eead
Rajan Sharma <raj@Rajans-MacBook-Air.local>
2024-07-03 23:08:39 -0500

    Use right sound file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import GameComponent from './GameComponent';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [parameter, setParameter] = useState(0);
+
+  useEffect(() => {
+    // Increment parameter every minute
+    const intervalId = setInterval(() => {
+      setParameter(prevParam => prevParam + 1);
+    }, 60000); // 60000 ms = 1 minute
+
+    return () => clearInterval(intervalId); // Cleanup interval on unmount
+  }, []);
+
   return (
-    <GameComponent parameter="moon.ogg" /> 
+    <GameComponent parameter={parameter % 2 == 0 ? "moon.ogg" : "zelda.ogg"}/> 
   );
 }
 
